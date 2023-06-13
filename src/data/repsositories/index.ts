1cb71d52c533c0ba44dc89a1cc6232a59e2d7458
markleungtofu <128244874+markleungtofu@users.noreply.github.com>
2023-06-13 21:45:38 +0800

    feature: adding repository to the client data

--- src/data/repsositories/index.ts
+++ src/data/repsositories/index.ts
@@ -1,5 +1,9 @@
+import { ClientModelRepository } from "./client.repository";
+
 /**
  * @const {Array} repositories - array of repositories offered by this application
  * @since 0.0.1
  */
-export const repositories = [];
+export const repositories = [
+    ClientModelRepository,
+];
