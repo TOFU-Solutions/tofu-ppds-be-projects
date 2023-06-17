a1daa0ea11772b1338744ae8ef585dd7af1edf4f
markleungtofu <128244874+markleungtofu@users.noreply.github.com>
2023-06-17 16:28:51 +0800

    update: decouple the project data object from client

--- src/data/models/client.model.ts
+++ src/data/models/client.model.ts
@@ -1,7 +1,4 @@
-import { Prop } from '@nestjs/mongoose';
-import { isUUID } from 'class-validator';
-import { DesignEntity } from 'src/business/entities/design.entity';
-import { ProjectEntity } from 'src/business/entities/project.entity';
+import { Prop, Schema } from '@nestjs/mongoose';
 import { i18n } from 'src/utils/entities/i18n.generic';
 import { BaseIndestructableModel } from 'src/utils/generics/model-undestructable.generic';
 
@@ -14,6 +11,9 @@ import { BaseIndestructableModel } from 'src/utils/generics/model-undestructable
  * @extends BaseIndestructableModel
  * @author Mark Leung <leungas@gmail.com>
  */
+@Schema({
+  collection: 'clients',
+})
 export class ClientModel extends BaseIndestructableModel {
   /**
    * @property {string} code - the code of the client
@@ -25,74 +25,6 @@ export class ClientModel extends BaseIndestructableModel {
     required: true,
   })
   code: string;
-
-  /**
-   * @property {DesignEntity[]} designs - the designs as part of this project/season
-   * @since 0.0.1
-   */
-  @Prop({
-    type: [
-      {
-        artifacts: [
-          {
-            description: {
-              type: String,
-              required: false,
-            },
-            id: {
-              type: String,
-              required: true,
-              validate: [
-                (v: string) => isUUID(v, '4'),
-                'Valid needs to be an UUID',
-              ],
-            },
-            label: {
-              type: String,
-              required: false,
-            },
-            type: {
-              type: String,
-              required: true,
-            },
-          },
-        ],
-        code: {
-          type: String,
-        },
-        commited: {
-          type: Boolean,
-          default: false,
-        },
-        designer: {
-          type: String,
-          required: true,
-        },
-        description: [
-          {
-            locale: {
-              type: String,
-              required: true,
-              validate: [
-                (v: string) => v.length === 2,
-                'The locale string must be 2 characters and comply to ISO-689-1',
-              ],
-            },
-          },
-        ],
-        name: {
-          type: String,
-          required: true,
-        },
-        tags: {
-          type: [String],
-          default: [],
-        },
-      },
-    ],
-    default: [],
-  })
-  designs: DesignEntity[];
 
   /**
    * @property {i18n[]} description - the description of the client
@@ -159,83 +91,6 @@ export class ClientModel extends BaseIndestructableModel {
     required: false,
   })
   owner: string;
-
-  /**
-   * @property {ProjectEntity[]} projects - the projects this client has
-   * @since 0.0.1
-   */
-  @Prop({
-    type: [
-      {
-        id: {
-          type: String,
-          required: true,
-          unique: true,
-        },
-        collection: {
-          type: String,
-          required: true,
-        },
-        createdOn: {
-          type: Date,
-          default: Date.now,
-          required: true,
-        },
-        description: [
-          {
-            locale: {
-              type: String,
-              required: true,
-              validate: [
-                (v: string) => v.length === 2,
-                'locale must be 2 characters long',
-              ],
-            },
-            value: {
-              type: String,
-              required: false,
-            },
-          },
-        ],
-        lastUpdatedOn: {
-          type: Date,
-          default: Date.now,
-          required: true,
-        },
-        name: {
-          type: String,
-          required: true,
-        },
-        owner: {
-          type: String,
-          required: false,
-        },
-        season: {
-          type: String,
-          required: false,
-        },
-        status: {
-          type: String,
-          enum: ['ACTIVE', 'ARCHIVED'],
-          default: 'ACTIVE',
-          required: true,
-        },
-        tags: {
-          type: [String],
-          required: false,
-          default: [],
-        },
-        year: {
-          type: Number,
-          required: false,
-        },
-      },
-    ],
-    default: [],
-    index: true,
-    required: false,
-  })
-  projects: ProjectEntity[];
 
   /**
    * @property {string} workspace - the workspace this client belongs to
