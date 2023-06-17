7a2c2cd67872f3eee4822e6a642ab13d2fb85e3d
markleungtofu <128244874+markleungtofu@users.noreply.github.com>
2023-06-17 16:26:16 +0800

    update: adding schemas into the data module

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -3,6 +3,10 @@ import { repositories } from './repsositories';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClientModel } from './models/client.model';
 import { ClientModelSchema } from './schemas/client.schema';
+import { ProjectModel } from './models/project.model';
+import { ProjectModelSchema } from './schemas/project.schema';
+import { DesignModel } from './models/design.model';
+import { DesignModelSchema } from './schemas/design.schema';
 
 /**
  * @module DataModule
@@ -19,6 +23,20 @@ import { ClientModelSchema } from './schemas/client.schema';
         name: ClientModel.name,
         useFactory: () => {
           const schema = ClientModelSchema;
+          return schema;
+        },
+      },
+      {
+        name: DesignModel.name,
+        useFactory: () => {
+          const schema = DesignModelSchema;
+          return schema;
+        },
+      },
+      {
+        name: ProjectModel.name,
+        useFactory: () => {
+          const schema = ProjectModelSchema;
           return schema;
         },
       },
